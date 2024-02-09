4b94677c3722b1222197434c9696fe163f2a3960
raelenecasey <raecasey@gmail.com>
2024-02-09 22:29:31 +0000

    Create dep_im_309h.sh

---
+++ dep_im_309h.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#create folder withing current directory to hold newly created jpegs
+mkdir 309height_jpgs
+
+#batch command to set pixel width to 309 and height to match aspect ratio of original. Using ffmpeg. Exporting to new folder created. all instances of jpeg extensions are listed
+for file in *.jpg; do ffmpeg -i "$file" -vf scale="-1:309" "./309height_jpgs/${file%.jpg}_309h.jpg"; done
+for file in *.JPG; do ffmpeg -i "$file" -vf scale="-1:309" "./309height_jpgs/${file%.JPG}_309h.jpg"; done
+for file in *.jpeg; do ffmpeg -i "$file" -vf scale="-1:309" "./309height_jpgs/${file%.jpeg}_309h.jpg"; done
+for file in *.JPEG; do ffmpeg -i "$file" -vf scale="-1:309" "./309height_jpgs/${file%.JPEG}_309h.jpg"; done
+for file in *.jp2; do ffmpeg -i "$file" -vf scale="-1:309" "./309height_jpgs/${file%.jp2}_309h.jp2"; done
+for file in *.JP2; do ffmpeg -i "$file" -vf scale="-1:309" "./309height_jpgs/${file%.JP2}_309h.jp2"; done
