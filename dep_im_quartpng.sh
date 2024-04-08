bce829779b1504b751df835bd1100b7e538e1676
raelenecasey <raecasey@gmail.com>
2024-04-08 12:47:11 +0100

    Update dep_im_quartpng.sh
    include shebang

--- dep_im_quartpng.sh
+++ dep_im_quartpng.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+#script written by Raelene Casey 2024
+
 for file in *.jpg; do ffmpeg -i "$file" -vf scale="iw/4:ih/4" "./halved_jpgs/${file%.jpg}_quart.png"; done
 for file in *.JPG; do ffmpeg -i "$file" -vf scale="iw/4:ih/4" "./halved_jpgs/${file%.JPG}_quart.png"; done
 for file in *.jpeg; do ffmpeg -i "$file" -vf scale="iw/4:ih/4" "./halved_jpgs/${file%.jpeg}_quart.png"; done
