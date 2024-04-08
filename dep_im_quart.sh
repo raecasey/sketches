59e66b944733bf35e78ea3158de96a7563b71d8f
raelenecasey <raecasey@gmail.com>
2024-04-08 13:02:08 +0100

    Create dep_im_quart.sh
    new script for reducing a batch of jpegs to a quarter of their original size

---
+++ dep_im_quart.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#create folder withing current directory to hold newly created jpegs
+mkdir quart_jpgs
+
+#batch command to halve pixel width and height of each file using ffmpeg and export to new folder created. all instances of jpeg extensions are listed
+for file in *.jpg; do ffmpeg -i "$file" -vf scale="iw/4:ih/4" "./quart_jpgs/${file%.jpg}_half.jpg"; done
+for file in *.JPG; do ffmpeg -i "$file" -vf scale="iw/4:ih/4" "./quart_jpgs/${file%.JPG}_half.jpg"; done
+for file in *.jpeg; do ffmpeg -i "$file" -vf scale="iw/4:ih/4" "./quart_jpgs/${file%.jpeg}_half.jpg"; done
+for file in *.JPEG; do ffmpeg -i "$file" -vf scale="iw/4:ih/4" "./quart_jpgs/${file%.JPEG}_half.jpg"; done
+for file in *.jp2; do ffmpeg -i "$file" -vf scale="iw/4:ih/4" "./quart_jpgs/${file%.jp2}_half.jp2"; done
+for file in *.JP2; do ffmpeg -i "$file" -vf scale="iw/4:ih/4" "./quart_jpgs/${file%.JP2}_half.jp2"; done
