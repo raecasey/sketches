a04f43b26a5d6e265d4b2cd08c931967357af891
raelenecasey <raecasey@gmail.com>
2024-04-08 12:44:27 +0100

    Create dep_im_quartpng.sh
    new script making images a quarter of original size and outputting from jpeg to png

---
+++ dep_im_quartpng.sh
@@ -0,0 +1,6 @@
+for file in *.jpg; do ffmpeg -i "$file" -vf scale="iw/4:ih/4" "./halved_jpgs/${file%.jpg}_quart.png"; done
+for file in *.JPG; do ffmpeg -i "$file" -vf scale="iw/4:ih/4" "./halved_jpgs/${file%.JPG}_quart.png"; done
+for file in *.jpeg; do ffmpeg -i "$file" -vf scale="iw/4:ih/4" "./halved_jpgs/${file%.jpeg}_quart.png"; done
+for file in *.JPEG; do ffmpeg -i "$file" -vf scale="iw/4:ih/4" "./halved_jpgs/${file%.JPEG}_quart.png"; done
+for file in *.jp2; do ffmpeg -i "$file" -vf scale="iw/4:ih/4" "./halved_jpgs/${file%.jp2}_quart.png"; done
+for file in *.JP2; do ffmpeg -i "$file" -vf scale="iw/4:ih/4" "./halved_jpgs/${file%.JP2}_quart.png"; done
