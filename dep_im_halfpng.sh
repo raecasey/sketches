f9fa9fbef2acc1a9e3287d7a41fe6f2993a85c80
raelenecasey <raecasey@gmail.com>
2024-04-08 12:42:26 +0100

    Create dep_im_halfpng.sh
    a new version of dev_im_half where the output is png

---
+++ dep_im_halfpng.sh
@@ -0,0 +1,6 @@
+for file in *.jpg; do ffmpeg -i "$file" -vf scale="iw/2:ih/2" "./halved_jpgs/${file%.jpg}_half.png"; done
+for file in *.JPG; do ffmpeg -i "$file" -vf scale="iw/2:ih/2" "./halved_jpgs/${file%.JPG}_half.png"; done
+for file in *.jpeg; do ffmpeg -i "$file" -vf scale="iw/2:ih/2" "./halved_jpgs/${file%.jpeg}_half.png"; done
+for file in *.JPEG; do ffmpeg -i "$file" -vf scale="iw/2:ih/2" "./halved_jpgs/${file%.JPEG}_half.png"; done
+for file in *.jp2; do ffmpeg -i "$file" -vf scale="iw/2:ih/2" "./halved_jpgs/${file%.jp2}_half.png"; done
+for file in *.JP2; do ffmpeg -i "$file" -vf scale="iw/2:ih/2" "./halved_jpgs/${file%.JP2}_half.png"; done
